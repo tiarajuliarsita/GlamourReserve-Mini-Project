a4cc7b0168243492778c6a0cf6d0c258555174c5
tiarajuliarsita <tiarajuliarsita@gmail.com>
2023-10-31 11:29:39 +0800

    fix:fixing port

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
 	routes.UserRoutes(app, database.DB)
 	routes.ServicesRoutes(app, database.DB)
 	routes.BookingRoutes(app, database.DB)
+
 	
 	app.Logger.Fatal(app.Start(fmt.Sprintf(":%d", appCfg.APPPORT)))
 }
